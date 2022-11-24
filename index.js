5ac5831fed7db0c9b8bb62b0fb0f8e5164513779
shady-mungai <shadrackmungai10@gmail.com>
2022-11-24 09:42:52 +0300

    Completed Assignment on control flow labs

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(numberOfFeet) {
   // Write your code here!
+  if (numberOfFeet <= 400) {
+    return "This one is on me!";
+  } else if (numberOfFeet > 2000 && numberOfFeet < 2499) {
+    return "I will gladly take your thirty bucks.";
+  }
+    else if (numberOfFeet > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+
+   return city == "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipReceived) {
   // Write your code here!
+  switch (tipReceived) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+
+    case "not as generous":
+     return "Thank you.";
+      break;
+    default: return "Bye."
+    
+  }
 }
